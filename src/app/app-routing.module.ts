c77f4021ea4544dcc56ebb11ef50419b9bf29a39
Lucca Jiménez Könings <development@jimkoen.com>
2023-07-17 05:38:50 +0200

    add Intersection Observer example
    Signed-off-by: Lucca Jiménez Könings <development@jimkoen.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {
+  IntersectionObserverExampleComponent
+} from "./examples/intersection-observer-example/intersection-observer-example.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "intersectionObserver",
+    component: IntersectionObserverExampleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
