ad907d6fce05fa1d3ac1e23754cb0fb1315bc1c8
Lucca Jiménez Könings <development@jimkoen.com>
2023-07-17 06:03:51 +0200

    add short overview to app component; add styles for app component
    Signed-off-by: Lucca Jiménez Könings <development@jimkoen.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <!--div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Links to different categories: </h2>
-    <ul>
-      <li>
-        <h2><a  target="_blank" rel="noopener" href="https://angular.io/tutorial">Intersection Observer API</a></h2>
-      </li>
-    </ul!-->
-    <router-outlet></router-outlet>
+    <header>
+        <h1>Angular Playground</h1>
+    </header>
+
+    <main>
+      <section>
+        <h2>Categories</h2>
+        <article>
+          <h3>Angular API Experiments</h3>
+          <p>Explore Angular APIs through these interactive experiments.</p>
+        </article>
+
+        <article>
+          <h3>Web API Experiments</h3>
+          <p>Discover the capabilities of different Web APIs with these interactive examples.</p>
+        </article>
+      </section>
+    </main>
+
+    <footer>
+      <p>&copy; 2023 Lucca Jiménez Könings. All rights reserved.</p>
+    </footer>
   `,
-  styles: []
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Playground';
