5c9cdd76b57d15208fdb8ca6d8ce1d299e79d514
Lucca Jiménez Könings <development@jimkoen.com>
2023-08-04 11:39:47 +0200

    add Angular Material + pocketbase table demo component that uses Angular Material
    Signed-off-by: Lucca Jiménez Könings <development@jimkoen.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClient} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import {HttpClient} from "@angular/common/http";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [HttpClient],
   bootstrap: [AppComponent]
---
+++ src/app/examples/pocketbase/angular-material/table-demo/table-demo.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TableDemoComponent } from './table-demo.component';
+
+describe('TableDemoComponent', () => {
+  let component: TableDemoComponent;
+  let fixture: ComponentFixture<TableDemoComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [TableDemoComponent]
+    });
+    fixture = TestBed.createComponent(TableDemoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
